f1951c75b3e17d6683b23d4f9ed9b1703ff33d7e
BRAXTONBUTLER2 <braxtonbutler@byui.edu>
2025-01-15 19:57:19 -0700

    done up to prep 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       Random randomGenerator = new Random();
+       int magicNumber = randomGenerator.Next(1, 101);
+
+
+       int guess = -1;
+
+       while (guess != magicNumber) 
+       {
+
+        Console.Write(" Hello! and welcome to my GUESS THE MAGIC NUMBER GAME,What is your guess?");
+        guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("the magic number is Higher");
+
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("the Magic number is lower");
+
+        }
+        else {
+            Console.WriteLine("You guessed it, great job");
+        }
+
+
+       }
     }
+
+  
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.Write("Hello and welcome to my program! ");  // Fixed missing semicolon
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.WriteLine($"Hello {firstname}");
+
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Your favorite number is {number}");
+
+        int square = SquareNumber(number);
+        DisplayResult(firstname, square);
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
