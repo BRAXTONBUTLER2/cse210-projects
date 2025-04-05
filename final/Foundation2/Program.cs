09f13d0e8ba22227b7b13192e0a335e3892a64f0
BRAXTONBUTLER2 <braxtonbutler@byui.edu>
2025-04-05 11:26:21 -0600

    smallchanges

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -9,8 +9,8 @@ class Program
         Customer customer1 = new Customer("John McEnroe", address1);
 
         Order order1 = new Order(customer1);
-        order1.AddProduct(new Product("Tennis Shoes", "TS202", 100.0, 2));
-        order1.AddProduct(new Product("Tennis Balls", "TB303", 8.0, 4));
+        order1.AddProduct(new Product("Track Shoes", "TS202", 100.0, 2));
+        order1.AddProduct(new Product("Basketballs Balls", "TB303", 8.0, 4));
 
         Console.WriteLine(order1.GeneratePackingLabel());
         Console.WriteLine(order1.GenerateShippingLabel());
@@ -21,9 +21,9 @@ class Program
         Customer customer2 = new Customer("Roger Federer", address2);
 
         Order order2 = new Order(customer2);
-        order2.AddProduct(new Product("Tennis Bag", "TB101", 75.0, 1));
-        order2.AddProduct(new Product("Overgrip Rolls", "OG999", 5.0, 5));
-        order2.AddProduct(new Product("Socks (3-pack)", "SO111", 20.0, 2));
+        order2.AddProduct(new Product("Skateboard", "TB101", 75.0, 1));
+        order2.AddProduct(new Product("Skate tool 3 pack", "OG999", 5.0, 5));
+        order2.AddProduct(new Product("Nerds gummy clusters", "SO111", 20.0, 2));
 
         Console.WriteLine(order2.GeneratePackingLabel());
         Console.WriteLine(order2.GenerateShippingLabel());
